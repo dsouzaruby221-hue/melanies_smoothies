d9dac97be17dd82c3356b34a7e377423e695b1c2
dsouzaruby221-hue <dsouzaruby221@gmail.com>
2025-09-26 15:34:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,10 +32,14 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
-   # my_insert_stmt = f""" insert into smoothies.public.orders(ingredients, name_on_order)
+    #my_insert_stmt = f""" insert into smoothies.public.orders(ingredients, name_on_order)
             #values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
-    my_insert_stmt = f""" INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('{ingredients}', '{name_on_order}')"""
-    #st.write(my_insert_stmt)
+    #my_insert_stmt = f""" INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('{ingredients}', '{name_on_order}')"""
+    session.table("smoothie_orders").insert(
+    values=[(ingredients, name_on_order)],
+    columns=["ingredients", "name_on_order"]
+)
+    st.write(my_insert_stmt)
     #st.stop()
 
     time_to_insert = st.button('Submit order')
